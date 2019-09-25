82c7c4b380c3188930897216ff83b8e81f14d5c9
Ejaz Ansari <ejazahemed999@gmail.com>
2019-09-25 16:50:14 +0530

    Component added | test cases started

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -3,7 +3,7 @@
 		<h1>Portfolio Manager2</h1>
 		<!-- <h2>Transactions details of the selected portfolio.</h2> -->
 		
-		<div>
+		<div style="padding: 20px">
 			<b-table hover 
 				:items="items" 
 				:fields="fields"
@@ -22,33 +22,62 @@ export default {
 	data () {
 		return {
 			fields: [
-				{ key: 'name', label: 'Name' },
+				{ key: 'name', label: 'Name', class: ['text-left'] },
 				{ key: 'holdings_count', label: 'Number of Holdings'},
 				{ key: 'last_modified', label: 'Last Modified'}
 			],
 			items: [],
-			gotoDetails: (item) => {
-				console.log('clicked', item.id);
-				this.$router.push({ name: 'Portfolio_detail', params: {}});
-			}
+			portfolios: null,
+			securities: null
+		}
+	},
+	methods: {
+		gotoDetails: function(item, index){
+			console.log('clicked', item.id, index);
+			this.$router.push({ name: 'Portfolio_detail', params: {
+				portfolio: this.portfolios[index],
+				securities: this.securities
+			}});
+		},
+		getProtfolios: function(){
+			return new Promise((resolve, reject) => {
+	            axios.get('https://8j4rtfdbq0.execute-api.ap-south-1.amazonaws.com/dev/portfolios')
+		        .then(resp => {
+		        	this.portfolios = resp.data["Portfolios"];
+		        	resolve(this.portfolios);
+		        })
+		        .catch(err => {
+		        	reject(err);
+		        })
+	        })
+		},
+		getSecurities: function(){
+			return new Promise((resolve, reject) => {
+	            axios.get('https://8j4rtfdbq0.execute-api.ap-south-1.amazonaws.com/dev/securities')
+		        .then(resp => {
+		        	this.securities = resp.data["Securities"];
+		        	resolve(this.securities);
+		        })
+		        .catch(err => {
+		        	reject(err);
+		        })
+	        })
 		}
 	},
 	mounted () {
-		console.log('mounted');
 
 		//protfolio fetch
-    	axios.get('https://8j4rtfdbq0.execute-api.ap-south-1.amazonaws.com/dev/portfolios')
-        .then(portfolioRes => {
+    	this.getProtfolios().then(portfolios => {
 
-        	window.port = portfolioRes.data;
+        	window.port = portfolios;
 
 			//securities fetch
-    		axios.get('https://8j4rtfdbq0.execute-api.ap-south-1.amazonaws.com/dev/securities')
-	        .then(securitiesRes => {
+    		this.getSecurities().then(securities => {
 
-	        	window.sec = securitiesRes.data;
+	        	window.sec = securities;
+
 	        	try{
-		        	portfolioRes.data.Portfolios.forEach(p => {
+		        	portfolios.forEach(p => {
 						let maxDate = '-';
 
 		        		let sorted = p["Transactions"].sort(function sortDates(a, b){
@@ -80,74 +109,5 @@ export default {
 	}
 }
 
-
-//console calculation to be applied
-
-
-var selectedDate = "2011-11-30";
-var items = [];
-function getShares(poprtfolio, allSecs){
-
-    poprtfolio["Transactions"].forEach((T) => {
-        securities = search(T['SecurityId'], '_Id', allSecs);
-    
-        //calc at transaction date
-        var value = searchPrice(T.Date, 'EndDate', securities['HistoryDetail']);
-//         if(value){
-     
-            T.name = securities['name'];
-            T.price = value['Value'];
-            T.shares = parseFloat(T.Amount) / parseFloat(T.price);
-            T.shares = floorFigure(T.shares);
-            T.selectedDate = {
-                shares: 0,
-                price: 0,
-                amount: 0
-            };
-//         }
-
-        //calc at selected date
-        var res = searchPrice(selectedDate, 'EndDate', securities['HistoryDetail']);
-        T.selectedDate.shares = T.shares;
-        T.selectedDate.price = res['Value'];
-        T.selectedDate.amount = parseFloat(T.selectedDate.shares) * parseFloat(T.selectedDate.price);
-        T.selectedDate.amount = T.selectedDate.amount.toFixed(2);        
-
-        items.push(T);
-    })
-}
-
-function search(term, attr, arr){
-    for (var i=0; i < arr.length; i++) {
-        if (arr[i][attr] === term) {
-            return arr[i];
-        }
-    }
-}
-
-function searchPrice(term, attr, arr){
-    var res = null;
-    for (var i=0; i < arr.length; i++) {
-        if (arr[i][attr] === term) {
-            if(arr[i]['Value']){
-                res = arr[i];
-            }else{
-
-                //get most prior selectedDate
-            }
-            break;
-        }
-    }
-    
-    return res;
-}
-
-function floorFigure(figure, decimals){
-    if (!decimals) decimals = 2;
-    var d = Math.pow(10,decimals);
-    return (parseInt(figure*d)/d).toFixed(decimals);
-};
-
-// getShares(port['Portfolios'][0], sec['Securities']);
 
 </script>
--- src/components/Portfolio_detail.vue
+++ src/components/Portfolio_detail.vue
@@ -3,11 +3,32 @@
 		<h1>Portfolio Manager</h1>
 		<h2>Transactions details of the selected portfolio.</h2>
 		
-		<div>
+		<div style="padding: 20px">
+			<div class="text-right" style="padding: 10px;">
+				Portfolio value by date:
+				<input type="date" v-model="selectedDate" @change="getItems()">
+			</div>
 			<b-table hover 
 				:items="items" 
 				:fields="fields"
-			>
+				foot-clone
+				 >
+
+				<template v-slot:head(selectedDateCombined)="data">
+			        <span class="text-info">
+			        	<div>Selected date</div>
+				        {{ data.label }}
+				    </span>
+			    </template>
+				<template v-slot:foot(name)="data">
+					<span class="text-danger">Total Portfolio value:</span>
+				</template>
+				<template v-slot:foot(selectedDateCombined)="data">
+					<span class="text-danger">{{ total }}</span>
+				</template>
+				<template v-slot:foot()="data">
+			        
+			    </template>					
 			</b-table>
 		</div>
 	</div>
@@ -15,19 +36,84 @@
 
 <script>
 import axios from 'axios'
+import { helper } from '@/helper'
 
 export default {
+	mixins: [helper],
 	name: 'portfolio',
 	data () {
 		return {
 			fields: [
-				{ key: 'name', label: 'Name' },
+				{ key: 'name', label: 'Name', class: ['text-left']},
+				{ key: 'Date', label: 'Transaction Date', class: ['text-left']},
+				{ key: 'Type', label: 'Transaction Type', class: ['text-left']},
+				{ key: 'Amount', label: 'Amount', class: ['text-right']},
+				{ key: 'price', label: 'Price', class: ['text-right']},
+				{ key: 'shares', label: 'Shares', class: ['text-right']},
+				{ key: 'selectedDateCombined', label: 'Shares | Price | Amount', class: ['text-right']},
 			],
-			items: []
+			items: [],
+			selectedPortfolio: null,
+			securities: null,
+			selectedDate: '2011-11-30',
+			total: 0
+		}
+	},
+	methods: {
+		getItems: function(){
+
+			// make a fresh entry
+			this.items = [];
+			this.total = 0;
+			// loop through each transaction
+		    this.selectedPortfolio["Transactions"].forEach((T) => {
+
+		    	// get specific security by id
+		        let security = this.findInArray(T['SecurityId'], '_Id', this.securities);
+		    
+		        //calculate at transaction date
+		        let value = this.findPriceByDate(T.Date, 'EndDate', security['HistoryDetail']);
+		        T.name = security['name'];
+		        T.price = parseFloat(value['Value']).toFixed(2);
+		        T.shares = parseFloat(T.Amount) / parseFloat(T.price);
+		        T.shares = this.floorFigure(T.shares);
+		        T.selectedDate = {
+		            shares: 0,
+		            price: 0,
+		            amount: 0
+		        };
+
+		        //calc at selected date
+		        let res = this.findPriceByDate(this.selectedDate, 'EndDate', security['HistoryDetail']);
+		        T.selectedDate.shares = T.shares;
+		        T.selectedDate.price = parseFloat(res['Value']).toFixed(2);
+		        T.selectedDate.amount = parseFloat(T.selectedDate.shares) * parseFloat(T.selectedDate.price);
+		        T.selectedDate.amount = T.selectedDate.amount.toFixed(2);        
+		        T.selectedDateCombined = T.selectedDate.shares + ' | ' + T.selectedDate.price + ' | ' + T.selectedDate.amount;
+		        // push computed item row
+		        this.items.push(T);
+
+		    })
+
+	        this.items.forEach((mi) => {
+	        	console.log(mi.selectedDate.amount);
+	        	this.total = parseFloat(this.total) + parseFloat(mi.selectedDate.amount);
+	        });
+	        this.total = this.total.toFixed(2);
+
+		    console.log("Final", this.items);
 		}
 	},
 	mounted () {
 		console.log('mounted');
+		this.selectedPortfolio = this.$route.params.portfolio;
+		this.securities = this.$route.params.securities;
+
+		if(this.selectedPortfolio && this.securities){
+			this.getItems();
+		}else{
+			this.$router.back();
+		}
 	}
 }
 </script>
